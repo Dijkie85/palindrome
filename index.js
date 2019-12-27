e8c4361e6c6ef94ac8d721de65d9b86a844dc326
David <davidmielnik@gmail.com>
2019-12-27 18:38:40 +0000

    Tested

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-modules.exports("Phrase");
+module.exports = Phrase;
+
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
 
 function Phrase(content) {
+  
   this.content = content;
+  
+  this.processedContent = function() {
+    return this.letters().toLowerCase();
+  }
+  
+  this.letters = function() {
+    const regexLetters = /[a-z]/gi
+    return (this.content.match(regexLetters) || []).join("");
+  };
+  
+  this.isPalindrome = function() {
+    return (this.processedContent() === this.processedContent().reverse());
+  };
 }
---
+++ test/test.js
@@ -0,0 +1,44 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", () => {
+  
+  describe("#isPalindrome", () => {
+    
+    it("returns false for non palindrome", () => {
+      let nonPalindrome = new Phrase("nosoypalindromo");
+      assert(!nonPalindrome.isPalindrome());
+    });
+    
+    it("returns true for simple palindrome", () => {
+      let palindrome = new Phrase("reconocer");
+      assert(palindrome.isPalindrome());
+    });
+    
+    it("returns true for palindrome with mixed case", () => {
+      let mixedCasePalindrome = new Phrase("ReConOcER");
+      assert(mixedCasePalindrome.isPalindrome());
+    });
+    
+    it("returns true for palindrome with punctuation", () => {
+      let punctuatedPalindrome = new Phrase("Ana lleva, al oso, la avellana!")
+      assert(punctuatedPalindrome.isPalindrome());
+    });
+    
+    
+  });
+  
+  describe("#letters", () => {
+    it("should return only letters", () => {
+      let punctuatedPalindrome = new Phrase("Ana.Lleva.Al Oso.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "AnaLlevaAlOso");
+    });
+    
+    it("should return blank if there are no letters", () => {
+      let noLetters = new Phrase("1234");
+      assert.strictEqual(noLetters.letters(), "");
+    });
+    
+  });
+  
+});
